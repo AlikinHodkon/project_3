8a502842e55a3e50247c3679068119c0645ac35f
Alikin Hodkon <kakto228030904@gmail.com>
2024-07-08 12:49:08 +0300

    first commit

---
+++ src/components/List.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState  } from 'react'
+import axios from 'axios'
+
+export default function List() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const API_key = "3f0661a993a1df77691d6bc7819ae9ed";
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`).then((response) => {
+        setData(response.data);
+      })
+    });
+  }, [])
+    
+  return (
+    <div className='flex flex-col w-full items-center justify-center'>
+      <h1 className='font-semibold text-[64px]'>Weather list</h1>
+        <div className='border border-black flex items-center'>
+          <p className='text-[32px]'>{data?.name} {data?.sys.country}</p>
+          <div className='flex flex-col ml-2 items-center'>
+            <p className='text-[32px]'>{data?.weather[0].main}</p>
+            <p className='text-[16px]'>{data?.weather[0].description}</p>
+          </div>
+          <div className='flex items-center flex-col ml-2'>
+            <h3 className='text-[32px]'>Wind</h3>
+            <div className='flex flex-row'>
+              <p className='text-[16px]'>{data?.wind.deg}</p>
+              <p className='text-[16px]'>{data?.wind.gust}</p>
+              <p className='text-[16px]'>{data?.wind.speed}</p>
+            </div>
+          </div>
+          <div className='flex items-center flex-col ml-2'> 
+            <h3 className='text-[32px]'>Sun</h3>
+            <div className='flex flex-row'>
+              <p className='text-[16px]'>{new Date(data?.sys.sunrise * 1000).toUTCString().slice(-12, -4)}</p>
+              <p className='text-[16px] ml-1'>{new Date(data?.sys.sunset * 1000).toUTCString().slice(-12, -4)}</p>
+            </div>
+          </div>
+          <div className='flex flex-col ml-2'>
+            <h3 className='text-[32px]'>Temperature</h3>
+            <div className='flex justify-center'>
+              <p className='text-[16px]'>{(Math.round((data?.main.temp-273.15) * 100) / 100).toString()}</p>
+              <p className='text-[16px] ml-2'>{(Math.round((data?.main.feels_like-273.15) * 100) / 100).toString()}</p>
+            </div>
+          </div>
+      </div>
+    </div>
+  )
+}
