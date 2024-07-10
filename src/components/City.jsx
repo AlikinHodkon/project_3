429cbea56501fac844101b82aeea73b7699758d8
Alikin Hodkon <kakto228030904@gmail.com>
2024-07-10 14:06:55 +0300

    remove_useless_files

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -2,7 +2,7 @@
 export default function City({city, removeCity, changeCity}) {
   return (
     <div className="flex justify-between border border-black">
-        <p className="ml-5 text-[24px]" onClick={() => {changeCity(city.name)}}>{city.name}</p>
+        <p className="ml-5 text-[24px] cursor-pointer" onClick={() => {changeCity(city.name)}}>{city.name}</p>
         <button className="mr-5 text-[24px] rounded-md h-5" onClick={() => {removeCity(city)}}><img className="h-10" src="./cross.svg" alt="" /></button>
     </div>
   )
