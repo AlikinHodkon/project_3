b6e30d2cc70669982be84e56a5cd5b59335f047c
Alikin Hodkon <kakto228030904@gmail.com>
2024-07-12 12:07:26 +0300

    add_table_version_and_fix_sidebar

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -23,6 +23,7 @@ export default function Body({position, settings, changeCity, realCity, changeBg
   useEffect(() => {
     setLoading(true);
     body.current.classList.add('hidden');
+    if (!document.getElementById('sideBar').classList.contains('hidden')){document.getElementById('sideBar').classList.add('hidden')}
     const API_key = "3f0661a993a1df77691d6bc7819ae9ed";
     if (position === null){
       localStorage.removeItem('lastCity');
@@ -91,45 +92,45 @@ export default function Body({position, settings, changeCity, realCity, changeBg
   }, [settings.temp])
   // bg-[#1B1B1D]
   return (
-    <div className='flex flex-col justify-center xl:flex-row min-h-[93vh] font-Mont text-white'>
+    <div className='flex flex-col justify-center xl:flex-row min-h-[100vh] xl:min-h-[93vh] font-Mont text-white'>
         {loading ? <Spinner /> : null}
         <div ref={body} className='flex flex-col w-full'>
-          <h1 className='xl:text-[64px] text-[24px] text-center font-bold'>Weather in {data?.name}</h1>
+          <h1 className='xl:text-[64px] text-[24px] md:text-[40px] text-center font-bold'>Weather in {data?.name}</h1>
             <div className='flex flex-wrap xl:flex-row flex-col-reverse'>
               <div className='flex flex-col xl:w-[20vw] min-h-[15vh] xl:ml-[5vw] xl:mr-0 mr-5 ml-5 p-1 glass rounded-[15px] xl:h-[25vh] items-center justify-center glass'>
-                <p className='xl:text-[40px] text-[30px] text-center'>{data?.name}</p>
-                <p className='xl:text-[40px] text-[20px] text-center'>{data?.sys.country}</p>
+                <p className='xl:text-[40px] text-[30px] md:text-[35px] text-center'>{data?.name}</p>
+                <p className='xl:text-[40px] text-[20px] md:text-[30px] text-center'>{data?.sys.country}</p>
               </div>
               <img className='xl:ml-auto xl:mr-[5vw] xl:min-w-[20vw] xl:max-h-[25vh] xl:object-contain object-contain h-[35vh]' src={icon}></img>
             </div> 
             <div className='min-w-[70vw] flex flex-wrap'>
-              <div className='flex flex-col glass xl:w-[20vw] w-full ml-[5vw] min-h-[15vh] xl:mr-0 mr-5 mt-5 xl:h-[25vh] rounded-[15px] items-center justify-center'>
-                <p className='xl:text-[40px] text-[30px] text-center'>{data?.weather[0].main}</p>
-                <p className='xl:text-[20px] text-[20px] text-center'>{data?.weather[0].description}</p>
+              <div className='flex flex-col glass xl:w-[20vw] w-full ml-[5vw] min-h-[15vh] xl:mr-0 mr-5 md:ml-5 xl:ml-[5vw] mt-5 xl:h-[25vh] rounded-[15px] items-center justify-center'>
+                <p className='xl:text-[40px] text-[30px] md:text-[35px] text-center'>{data?.weather[0].main}</p>
+                <p className='xl:text-[20px] text-[20px] md:text-[30px] text-center'>{data?.weather[0].description}</p>
               </div>
               <div ref={wind} className='flex items-center justify-center flex-col ml-5 xl:mr-0 min-h-[15vh] mr-5 glass rounded-[15px] w-full xl:w-[20vw] xl:h-[25vh] mt-5 hidden'>
-                <h3 className='xl:text-[40px] text-[30px]'>Wind</h3>
+                <h3 className='xl:text-[40px] md:text-[35px] text-[30px]'>Wind</h3>
                 <div className='flex xl:flex-col'>
-                  <p className='xl:text-[20px] text-[20px] xl:mr-0 mr-1 text-center'>deg: {data?.wind.deg} </p>
-                  <p className='xl:text-[20px] text-[20px] xl:mr-0 mr-1 text-center'>gust: {data?.wind.gust} </p>
-                  <p className='xl:text-[20px] text-[20px] xl:mr-0 mr-1 text-center'>spd: {data?.wind.speed} m/s</p>
+                  <p className='xl:text-[20px] md:text-[30px] text-[20px] xl:mr-0 mr-1 text-center'>deg: {data?.wind.deg} </p>
+                  <p className='xl:text-[20px] md:text-[30px] text-[20px] xl:mr-0 mr-1 text-center'>gust: {data?.wind.gust} </p>
+                  <p className='xl:text-[20px] md:text-[30px] text-[20px] xl:mr-0 mr-1 text-center'>spd: {data?.wind.speed} m/s</p>
                 </div>
               </div>
               <div ref={sun} className='flex items-center justify-center flex-col ml-5 xl:mr-0 min-h-[15vh] mr-5 glass rounded-[15px] w-full xl:w-[20vw] xl:h-[25vh] mt-5 hidden'> 
-                <h3 className='xl:text-[40px] text-[30px]'>Sun</h3>
+                <h3 className='xl:text-[40px] md:text-[35px] text-[30px]'>Sun</h3>
                 <div className='flex flex-row'>
-                  <div className='xl:text-[20px] text-[20px] text-center'>
+                  <div className='xl:text-[20px] md:text-[30px] text-[20px] text-center'>
                     <p>sunrise: {new Date(data?.sys.sunrise * 1000 + (h*60*60*1000)).toUTCString().slice(-12, -4)}</p>
                     <p>sunset: {new Date(data?.sys.sunset * 1000 + (h*60*60*1000)).toUTCString().slice(-12, -4)}</p>
                   </div>
                 </div>
               </div>
             </div>
-            <div ref={temp} className='flex flex-col ml-[5vw] mr-[5vw] mt-5 mb-2 xl:min-h-[25vh] min-h-[15vh] items-center justify-center glass rounded-[15px] hidden'>
-              <h3 className='xl:text-[40px] text-[30px] text-center'>Temperature</h3>
+            <div ref={temp} className='flex flex-col ml-[5vw] xl:ml-[5vw] md:ml-5 md:mr-5 xl:mb-0 mb-2 mr-[5vw] xl:mr-[5vw] mt-5 xl:min-h-[25vh] min-h-[15vh] items-center justify-center glass rounded-[15px] hidden'>
+              <h3 className='xl:text-[40px] text-[30px] md:text-[35px] text-center'>Temperature</h3>
               <div className='flex justify-center'>
-                <p className='xl:text-[20px] text-[20px] text-center'>real: {(Math.round((data?.main.temp-273.15) * 100) / 100).toString()} </p>
-                <p className='xl:text-[20px] text-[20px] text-center ml-2'>feels like: {(Math.round((data?.main.feels_like-273.15) * 100) / 100).toString()}</p>
+                <p className='xl:text-[20px] text-[20px] md:text-[30px] text-center'>real: {(Math.round((data?.main.temp-273.15) * 100) / 100).toString()} </p>
+                <p className='xl:text-[20px] text-[20px] md:text-[30px] text-center ml-2'>feels like: {(Math.round((data?.main.feels_like-273.15) * 100) / 100).toString()}</p>
               </div>
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -60,25 +60,25 @@ export default function Navbar({changeCity, settings, setSettings, openSideBar})
 
   const modalContent = (
     <div className="font-Mont flex flex-col items-center">
-      <h2 className="text-center font-bold xl:text-[64px] text-[40px]">Settings</h2>
-      <p className="text-center font-semibold xl:text-[32px] text-[20px]">Choose what kind of data do you want to display</p>
-      <ul className="flex flex-col xl:text-[32px] mt-5 mb-5 text-[20px] font-semibold xl:w-1/2 w-full pl-3 xl:pl-0 xl:pr-0 pr-3">
+      <h2 className="text-center font-bold xl:text-[64px] md:text-[52px] text-[40px]">Settings</h2>
+      <p className="text-center font-semibold xl:text-[32px] md:text-[26px] text-[20px]">Choose what kind of data do you want to display</p>
+      <ul className="flex flex-col xl:text-[32px] mt-5 mb-5 md:text-[26px] text-[20px] font-semibold xl:w-1/2 w-full pl-3 xl:pl-0 xl:pr-0 pr-3">
         <li className="flex xl:justify-between justify-center items-center mb-2 p-1 xl:flex-row flex-col">Wind<button onClick={() => {setSettings({wind: !settings.wind, sun: settings.sun, temp: settings.temp}); localStorage.setItem('settingsWind', JSON.stringify(!settings.wind)); changeWind()}} className="border-black border-[2px] rounded-xl xl:w-[70px] w-1/2">{wind}</button></li>
         <li className="flex xl:justify-between justify-center items-center mb-2 p-1 xl:flex-row flex-col">Sunset & Sunrise<button onClick={() => {setSettings({wind: settings.wind, sun: !settings.sun, temp: settings.temp}); localStorage.setItem('settingsSun', JSON.stringify(!settings.sun)); changeSun()}} className="border-black border-[2px] rounded-xl xl:w-[70px] w-1/2">{sun}</button></li>
         <li className="flex xl:justify-between justify-center items-center mb-2 p-1 xl:flex-row flex-col">Temperature<button onClick={() => {setSettings({wind: settings.wind, sun: settings.sun, temp: !settings.temp}); localStorage.setItem('settingsTemp', JSON.stringify(!settings.temp)); changeTemp()}} className="border-black border-[2px] rounded-xl xl:w-[70px] w-1/2">{temp}</button></li>
       </ul>
-      <button onClick={closeModal} className="border-[3px] p-1 border-black xl:text-[32px] text-[20px] font-semibold rounded-2xl">Close</button>
+      <button onClick={closeModal} className="border-[3px] p-1 border-black xl:text-[32px] md:text-[26px] text-[20px] font-semibold rounded-2xl">Close</button>
     </div>
   );
   return (
     <div className='flex max-h-[70px] text-white'>
-        <ul className='flex w-full text-[14px] xl:text-[24px] font-semibold font-Mont items-center justify-center'>
-            <li className="xl:mr-auto xl:ml-[5vw] xl:text-[32px]">Your Weather</li>
-            <li className="p-1 xl:mr-[5vw]"><input ref={inputCity} onKeyDownCapture={handleKeyPressed} type='text' className='h-1/2 outline-none bg-transparent text-[14px] max-w-[100px] xl:min-w-[30vw] xl:text-[32px] search placeholder:text-white pl-4 xl:pl-10' placeholder='Search City' /></li>
+        <ul className='flex w-full text-[14px] lg:text-[32px] xl:text-[24px] font-semibold font-Mont items-center justify-center'>
+            <li className="xl:mr-auto md:mr-auto lg:ml-[5vw] xl:ml-[5vw] md:text-[32px] xl:text-[32px]">Your Weather</li>
+            <li className="p-1 md:mr-[3vw] lg:mr-0 xl:mr-[5vw]"><input ref={inputCity} onKeyDownCapture={handleKeyPressed} type='text' className='h-1/2 outline-none bg-transparent text-[14px] md:min-w-[30vw] md:text-[32px] max-w-[100px] xl:min-w-[30vw] xl:text-[32px] search placeholder:text-white pl-4 md:pl-10 xl:pl-10' placeholder='Search City' /></li>
             {/* <li className="mr-[5vw]"><button className='border-[2px] rounded-xl p-1 border-white w-[6vw]'>Search</button></li> */}
-            <li className="xl:mr-[5vw] mr-1 h-full flex items-center justify-center cursor-pointer text-[14px] xl:text-[24px] hover:text-gray-700" onClick={() => {changeCity("")}}>Home</li>
-            <li className="xl:mr-[5vw] mr-1 h-full flex items-center justify-center cursor-pointer text-[14px] xl:text-[24px] hover:text-gray-700" onClick={() => {openSideBar()}}>Cities</li>
-            <li onClick={openModal} className="xl:mr-[5vw] h-full flex items-center justify-center cursor-pointer hover:text-gray-700">Settings</li>
+            <li className="xl:mr-[5vw] mr-1 md:mr-2 lg:mr-5 h-full flex items-center lg:text-[32px] justify-center cursor-pointer text-[14px] md:text-[24px] xl:text-[24px] hover:text-gray-700" onClick={() => {changeCity("")}}>Home</li>
+            <li className="xl:mr-[5vw] mr-1 md:mr-2 h-full lg:mr-5 flex items-center lg:text-[32px] justify-center cursor-pointer text-[14px] md:text-[24px] xl:text-[24px] hover:text-gray-700" onClick={() => {openSideBar()}}>Cities</li>
+            <li onClick={openModal} className="xl:mr-[5vw] md:mr-2 lg:mr-5 md:text-[24px] lg:text-[32px] h-full flex items-center justify-center cursor-pointer hover:text-gray-700">Settings</li>
         </ul>
         <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>{modalContent}</Modal>
     </div>
